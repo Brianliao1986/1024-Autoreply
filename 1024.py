c7261b3163c4a3b46d126b0ccda5e4a925a43337
Brian_Leu <24835513+Brianliao1986@users.noreply.github.com>
2021-01-24 12:05:34 +0800

    Update 1024.py

--- 1024.py
+++ 1024.py
@@ -259,7 +259,7 @@ if __name__ == "__main__":
             auto.browse()
             auto.getreply()
             auto.getmatch()
-            sleeptime=random.randint(250,400)
+            sleeptime=random.randint(1024,2048)
             au=auto.postreply()
             if au=='回复成功':
                 auto.debug('回复成功')
